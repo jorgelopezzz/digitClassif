421d5e9f23e244513cfdf359b2816b1205076c02
jorgelopezzz <jorgelopez.sp1@gmail.com>
2025-07-14 19:50:28 +0200

    Up to gradient descent

---
+++ main.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pandas as pd
+
+
+
+train_dataframe = pd.read_csv('data/train.csv')
+train_data = np.array(train_dataframe)
+
+test_dataframe = pd.read_csv('data/test.csv')
+test_data = np.array(test_dataframe)
+
+# m = number of cases to train our model
+# n = size of test case (image_size + label)
+m, n = train_data.shape
+
+test_data = test_data.T
+test_labels = test_data[0]
+test_images = test_data[1:n] / 255.
+
+train_data = train_data.T
+train_labels = train_data[0]
+train_images = train_data[1:n] / 255.
+
+def init_parameters():
+    W1 = np.random.rand(10,784) - 0.5 # generates a [10,784] array of numbers in [-0.5, 0.5]
+    b1 = np.random.rand(10,1) - 0.5 
+    W2 = np.random.rand(10,10) - 0.5
+    b2 = np.random.rand(10,1) - 0.5 
+    return W1, b1, W2, b2
+
+def ReLU(Z):
+    return np.maximum(Z,0)
+
+def ReLU_deriv(Z):
+    return Z > 0
+
+def softmax(Z):
+    A = np.exp(Z) / sum(np.exp(Z))
+    return A
+
+def forward_prop(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max()+1)) # zeros only
+    # Y.size = number of test cases
+    # Y.max = 9 (maximum of values in Y) this only works with numerical categories starting with zero
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    # first index = 0, 1, ..., Y.size-1
+    # second index = labels
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1/m * dZ2.dot(A1.T)
+    db2 = 1/m * np.sum(dZ2)
+    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
+    dW1 = 1/m * dZ1.dot(X.T)
+    db1 = 1/m * np.sum(dZ1)
+    return dW1, db1, dW2, db2
+
+def update_parameters(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+    return W1, b1, W2, b2
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return np.sum(predictions == Y) / Y.size
+
+def show_accuracy(i, A2, Y):
+    print("Iteration: ", i)
+    predictions = get_predictions(A2)
+    print(get_accuracy(predictions, Y))
+
+def gradient_descent(X, Y, alpha, iterations):
+    W1, b1, W2, b2 = init_parameters()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_parameters(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i % 10 == 0:
+            show_accuracy(i, A2, Y)
+    return W1, b1, W2, b2
+
+W1, b1, W2, b2 = gradient_descent(train_images, train_labels, 0.1, 500)
+
